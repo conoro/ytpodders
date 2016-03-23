e0fc220171bd315d1f8a59db536b873ec81e94b1
Conor ONeill <cwjoneill@gmail.com>
2016-03-23 08:21:44 +0000

    trying to use gb for vendoring to avoid dependency issues in the future

---
+++ src/ytpodders/dropboxing.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"bufio"
+	b64 "encoding/base64"
+	"encoding/json"
+	"errors"
+	"io"
+	"time"
+
+	"fmt"
+	"os"
+
+	"strings"
+
+	"github.com/stacktic/dropbox"
+)
+
+// Configuration from conf.json
+type Configuration struct {
+	ClientID     string `json:"clientid"`
+	ClientSecret string `json:"clientsecret"`
+	Token        string `json:"token"`
+}
+
+var db *dropbox.Dropbox
+var dropboxLink *dropbox.Link
+var err error
+
+func getDropboxFolder() (string, error) {
+
+	var clientid, clientsecret, token string
+	var dropboxFolder string
+	// Read configuration from conf.json
+	conffile, _ := os.Open("conf.json")
+	decoder := json.NewDecoder(conffile)
+	config := Configuration{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		fmt.Println("config error:", err)
+		return "", err
+	}
+
+	clientid = config.ClientID
+	clientsecret = config.ClientSecret
+	token = config.Token
+
+	// TODO: Check if Token defined in conf.json. If no, do longer OAuth flow and then save token to conf.json
+
+	// 1. Create a new dropbox object.
+	db = dropbox.NewDropbox()
+
+	// 2. Provide your clientid and clientsecret (see prerequisite).
+	db.SetAppInfo(clientid, clientsecret)
+
+	// This method will ask the user to visit an URL and paste the generated code.
+	//if err = db.Auth(); err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	// You can now retrieve the token if you want.
+	//token = db.AccessToken()
+	//fmt.Println(token)
+
+	// 3. Provide the user token.
+	db.SetAccessToken(token)
+
+	// 4. Send your commands.
+	// In this example, you will create a new folder named "demo".
+	//folder := "podcasts"
+	//if _, err = db.CreateFolder(folder); err != nil {
+	//	fmt.Printf("Error creating folder %s: %s\n", folder, err)
+	//} else {
+	//	fmt.Printf("Folder %s successfully created\n", folder)
+	//}
+
+	// Only use this if running on a Server, not your local PC. Because it uploads to Cloud and then Dropbox syncs back down. So double the bandwidth
+	// Do this by checking for the existence of (on Windows only obvs) %APPDATA%\Dropbox\host.db
+	// If it exists then read it and base64 decode the second line of its contents to get the root path of the Dropbox files for this user
+	// Then just do a local OS file copy to the right path instead of using db.UploadFile
+	if _, err = os.Stat(os.Getenv("LOCALAPPDATA") + "\\Dropbox\\host.db"); err == nil {
+		fmt.Printf("It's local baby!\n")
+
+		var dropboxDefFile *os.File
+		dropboxDefFile, err = os.Open(os.Getenv("LOCALAPPDATA") + "\\Dropbox\\host.db")
+		if err != nil {
+			return "", err
+		}
+		defer dropboxDefFile.Close()
+
+		scanner := bufio.NewScanner(dropboxDefFile)
+
+		// Skip first line
+		scanner.Scan()
+
+		// Dropbox Store path on second line
+		scanner.Scan()
+
+		// Base64 decode the second line to get the path
+		var sDec []byte
+		sDec, err = b64.StdEncoding.DecodeString(scanner.Text())
+		if err != nil {
+			return "", err
+		}
+		dropboxFolder = string(sDec)
+		fmt.Println(dropboxFolder)
+		return dropboxFolder, nil
+	}
+	return "remote", nil
+
+}
+
+// TODO: Add logging everywhere
+// TODO: Move all of the code below to working with a list of files
+// TODO: Should probably run the code inside a Dropbox folder when running locally to avoid the file copying (or have CMD file in Dropbox Dir)
+// TODO: Need to figure out Hierarchy (possibly just one directory per Sub)
+// TODO: Need to figure out clean naming for Windows filesystem e.g. remove underscores, hashes, colons, slashes etc (or escape them)
+// TODO: Figure out Windows Scheduler again
+
+// TODO: Need to add the filelength to the RSS file
+
+func copyLocallyToDropbox(srcFile string, destFolder string) error {
+
+	s, err := os.Open(srcFile)
+	if err != nil {
+		return err
+	}
+
+	defer s.Close()
+	destFileSplit := strings.Split(destFolder+srcFile, "\\")
+	destFile := destFileSplit[len(destFileSplit)-1]
+	err = os.MkdirAll(destFile, 0777)
+	if err != nil {
+		return err
+	}
+
+	d, err := os.Create(destFolder + srcFile)
+	if err != nil {
+		return err
+	}
+
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+	d.Close()
+	return nil
+}
+
+func copyRemotelyToDropbox(srcFile string, destPath string) error {
+	var rev string
+
+	fmt.Printf("It's remote baby!\n")
+
+	if _, err = db.UploadFile(srcFile, destPath, true, rev); err != nil {
+		fmt.Printf("Error uploading file: %s\n", err)
+		return err
+	}
+	fmt.Printf("File successfully uploaded\n")
+	return nil
+}
+
+// getDropboxURLwhenSyncComplete will keep trying to getDropboxURL() until either
+// we get a result from getDropboxURL() or the timeout expires
+func getDropboxURLWhenSyncComplete(destFile string) (string, error) {
+
+	// 2 minutes seems a reasonable timeout for an MP3 to upload from Local to Remote
+	timeout := time.After(2 * time.Minute)
+	tick := time.Tick(10000 * time.Millisecond)
+	// Keep trying until we're timed out or got a result or got an error
+	for {
+		select {
+		// Got a timeout! fail with a timeout error
+		case <-timeout:
+			return "", errors.New("timed out")
+		// Got a tick, we should check on getDropboxURL()
+		case <-tick:
+			dropboxURL, err := getDropboxURL(destFile)
+			// Error from getDropboxURL(), we should bail
+			if err == nil {
+				return dropboxURL, err
+			}
+			// getDropboxURL() didn't work yet, but it didn't fail, so let's try again
+			// this will exit up to the for loop
+		}
+	}
+}
+
+func getDropboxURL(destFile string) (string, error) {
+	// Need to get Download URL of the Dropbox file so I can add to rss.xml
+	if dropboxLink, err = db.Shares(destFile, false); err != nil {
+		fmt.Printf("%s: %s\n", destFile, err)
+		return "", err
+	}
+	s := strings.Split(dropboxLink.URL, "?")
+	dlLink := s[0] + "?raw=1"
+	fmt.Printf("MP3 Direct download URL is: %s\n", dlLink)
+	return dlLink, nil
+}
---
+++ src/ytpodders/ytpodders.go
@@ -0,0 +1,258 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/SlyMarbo/rss"
+	"github.com/gorilla/feeds"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+/*
+sqlite> .schema subscriptions
+CREATE TABLE subscriptions(
+ID INTEGER PRIMARY KEY,
+suburl CHAR(1024)
+);
+sqlite> .schema subscription_entries
+CREATE TABLE subscription_entries(
+subscription INTEGER,
+url TEXT,
+title TEXT,
+date TEXT,
+FOREIGN KEY(subscription) REFERENCES subscriptions(ID)
+);
+
+*/
+
+// INSERT INTO subscriptions(suburl) VALUES ("https://www.youtube.com/user/durianriders");
+// INSERT INTO subscriptions(suburl) VALUES ("https://www.youtube.com/channel/UCYdkEm-NjhS8TmLVt_qZy9g");
+// INSERT INTO subscriptions(suburl) VALUES ("https://www.youtube.com/user/sciguy14");
+// INSERT INTO subscriptions(suburl) VALUES ("https://www.youtube.com/channel/UCh8rjWtGCIAbwPrZb3Te8bQ");
+// INSERT INTO subscriptions(suburl) VALUES ("https://www.youtube.com/channel/UCSUi7O_Fg6SgwkF2W2Au2Zw");
+
+// YTSubscription is just the URL of each YouTuber you are subscribed to
+type YTSubscription struct {
+	SubID  int64  `db:"ID"`
+	SubURL string `db:"suburl"`
+}
+
+// YTSubscriptionEntry has info about each of the parsed and downloaded "episodes" from YouTube
+type YTSubscriptionEntry struct {
+	Subscription int64  `db:"subscription"`
+	URL          string `db:"url"`
+	Title        string `db:"title"`
+	Date         string `db:"date"`
+}
+
+// YTdl has info about the YouTube file itself from youtube-dl
+//type YTdl struct {
+//	Uploader string `json:"uploader"`
+//	Title    string `json:"title"`
+//}
+
+func main() {
+	//	var youtubeInfo YTdl
+	var dropboxFolder string
+	dropboxFolder, err = getDropboxFolder()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	vidcmd := "youtube-dl"
+	//vidcmd := "echo"
+
+	db, err := sqlx.Connect("sqlite3", "ytpodders.db")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// query
+	ytSubscriptions := []YTSubscription{}
+	err = db.Select(&ytSubscriptions, "SELECT ID, suburl FROM subscriptions")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	//fmt.Println(ytSubscriptions)
+
+	for _, subscription := range ytSubscriptions {
+
+		ytSubscriptionEntries := []YTSubscriptionEntry{}
+		err = db.Select(&ytSubscriptionEntries, "SELECT subscription, url, title, date FROM subscription_entries WHERE subscription=$1", subscription.SubID)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(subscription)
+		fmt.Println(ytSubscriptionEntries)
+
+		var feedURL string
+
+		split := strings.Split(subscription.SubURL, "/")
+		feedid := split[len(split)-1]
+		fmt.Println(feedid)
+
+		if strings.Contains(subscription.SubURL, "channel") {
+			feedURL = "https://www.youtube.com/feeds/videos.xml?channel_id=" + feedid
+		} else {
+			feedURL = "https://www.youtube.com/feeds/videos.xml?user=" + feedid
+		}
+		feed, error := rss.Fetch(feedURL)
+		if error != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(feed)
+
+		for _, item := range feed.Items {
+			fmt.Println(item.Title)
+			if RSSEntryInDB(item.Link, ytSubscriptionEntries) == false {
+				fmt.Println("Adding new RSS Entry")
+				tx := db.MustBegin()
+				tx.MustExec("INSERT INTO subscription_entries(subscription,url,title,date) VALUES($1,$2,$3,$4)", subscription.SubID, item.Link, item.Title, item.Date)
+				tx.Commit()
+
+				//TODO: Need to figure out what filename youtube-dl is generating here
+				args1 := []string{"-v", "--extract-audio", "--audio-format", "mp3", "-o", "./podcasts/%(uploader)s/%(title)s.%(ext)s", item.Link}
+				cmd := exec.Command(vidcmd, args1...)
+
+				var out bytes.Buffer
+				cmd.Stdout = &out
+				err = cmd.Run()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error: %v\n", err)
+					os.Exit(1)
+				}
+
+				//err = json.Unmarshal([]byte(out.String()), &youtubeInfo)
+				//if err != nil {
+				//	fmt.Fprintf(os.Stderr, "error: %v\n", err)
+				//	os.Exit(1)
+				//}
+
+				var ytdlPath string
+				scanner := bufio.NewScanner(&out)
+				for scanner.Scan() {
+					if strings.Contains(scanner.Text(), "[ffmpeg] Destination:") {
+						ytdlPath = strings.Split(scanner.Text(), ": ")[1]
+						// podcasts\Anthony Ngu\Universal Smart Home Remote  - Prototype Demo.mp3
+					}
+				}
+
+				mp3FileLocalStyle := ytdlPath
+				mp3FileRemoteStyle := "/" + strings.Replace(ytdlPath, "\\", "/", -1)
+				//youtubeUploader := strings.Split(ytdlPath, "\\")[1]
+				fmt.Println(mp3FileLocalStyle)
+				fmt.Println(mp3FileRemoteStyle)
+
+				getFileSize(mp3FileLocalStyle)
+
+				if dropboxFolder != "remote" {
+					err = copyLocallyToDropbox(mp3FileLocalStyle, dropboxFolder+"\\Apps\\YouTube2Podcast\\")
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "Copy to Local Dropbox Error: %v\n", err)
+						os.Exit(1)
+					}
+				} else {
+					err = copyRemotelyToDropbox("."+mp3FileRemoteStyle, mp3FileRemoteStyle)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "error: %v\n", err)
+						os.Exit(1)
+					}
+				}
+				var dropboxURL string
+				dropboxURL, err = getDropboxURLWhenSyncComplete(mp3FileRemoteStyle)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "error: %v\n", err)
+					os.Exit(1)
+				}
+				fmt.Println(dropboxURL)
+
+			}
+		}
+
+	}
+
+	// TODO: Need to create and update rss.xml
+	RSSFile, err := generateRSS(dropboxFolder + "\\Apps\\YouTube2Podcast\\")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(RSSFile)
+
+	RSSFileURL, err := getDropboxURL("rss.xml")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(RSSFileURL)
+
+}
+
+// RSSEntryInDB checks if we have already downloaded this "episode"
+func RSSEntryInDB(link string, dbentries []YTSubscriptionEntry) bool {
+	for _, b := range dbentries {
+		if b.URL == link {
+			return true
+		}
+	}
+	return false
+}
+
+func generateRSS(dropboxFolder string) (string, error) {
+	now := time.Now()
+	feed := &feeds.Feed{
+		Title:       "My YouTube Podcasts",
+		Link:        &feeds.Link{Href: "http://conoroneill.net"},
+		Description: "YouTube Videos converted to Podcasts",
+		Author:      &feeds.Author{Name: "YouTuber", Email: "youtuber@example.com"},
+		Created:     now,
+	}
+
+	feed.Items = []*feeds.Item{
+		{
+			Title:       "GINGER RUNNER LIVE 107b The Joe Grant Episode",
+			Link:        &feeds.Link{Href: "https://www.dropbox.com/s/d5vei33k0xcdndm/GINGER%20RUNNER%20LIVE%20%23107%20_%20The%20Joe%20Grant%20Episode-pYHbkSNAR7I.mp3?raw=1", Length: "83503158", Type: "audio/mpeg"},
+			Description: "GINGER RUNNER LIVE 107b The Joe Grant Episode",
+			Author:      &feeds.Author{Name: "YouTuber", Email: "youtuber@example.com"},
+			Created:     now,
+		},
+	}
+
+	rss, err := feed.ToRss()
+	if err != nil {
+		return "", err
+	}
+
+	rssOut := []byte(rss)
+
+	// TODO: Need to add remote upload of this file when not running on local PC
+	rssFile := dropboxFolder + "rss.xml"
+	err = ioutil.WriteFile(rssFile, rssOut, 0644)
+	if err != nil {
+		return "", err
+	}
+	return rssFile, nil
+}
+
+func getFileSize(srcFile string) (int64, error) {
+	fi, err := os.Stat(srcFile)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Printf("The file is %d bytes long", fi.Size())
+	return fi.Size(), nil
+}
